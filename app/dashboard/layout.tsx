2652726baabaf199ab74d730b6ce6527f04f3dd5
kujo205 <kuc8301@gmail.com>
2024-01-10 12:47:13 +0200

    feat: chapter 5 completed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,8 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
-        wsd sad sad sad dad sad asd sad sadasdasdsadsad
-        <div>what the fuck is going on man huh</div>
+        <div></div>
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
