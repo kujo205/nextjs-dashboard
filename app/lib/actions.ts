e8ddb0fc0a62e3ad0677c7d34e714c028909beba
kujo205 <kuc8301@gmail.com>
2024-01-12 22:16:21 +0200

    feat: chapter 14 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,25 +5,49 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-const formSchema = z.object({
-  id: z.string().optional(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string().optional(),
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
 });
 
-const CreateInvoiceSchema = formSchema.omit({ id: true, date: true });
-const UpdateInvoiceSchema = formSchema.omit({ id: true, date: true });
+const CreateInvoiceSchema = FormSchema.omit({ id: true, date: true });
+const UpdateInvoiceSchema = FormSchema.omit({ id: true, date: true });
 
 function revalidateAndRedirect(path: string) {
   revalidatePath(path);
   redirect(path);
 }
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
   const rawFormData = Object.fromEntries(formData.entries());
-  const { customerId, amount, status } = CreateInvoiceSchema.parse(rawFormData);
+  const validatedFields = CreateInvoiceSchema.safeParse(rawFormData);
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
